bb12f5bd6fb3f21d54f55af2e282ce0f2012030b
Oscar Serrano <oscarsy.ing.systems@hotmail.com>
2025-06-21 18:11:47 -0600

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,23 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'tarjeta-marco';
+  correo!: string;
+  cel!: string;
+  nombre!: string;
+  puesto!: string;
+  direccion!: string;
+  empresa!: string;
+
+  constructor() {
+    this.correo = 'legal@naxos-technology.com.pa';
+    this.cel = '+52 56 10 40 35 68';
+    this.nombre = 'Marco Polo Castro Perez';
+    this.puesto = 'General Counsel';
+    this.direccion = 'Costa del Este, PH Financial Park Piso 18 Oficina 1803';
+    this.empresa = 'NAXOS TECHNOLOGY LIMITED';
+  }
 }
